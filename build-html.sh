1a5ecdb1cc34573dbd46d87dcb6f59f3ea22601a
Yongfu Liao <liao961120@gmail.com>
2023-11-14 10:50:27 +0800

    tweak html

--- build-html.sh
+++ build-html.sh
@@ -1,3 +1,6 @@
-pandoc main.md -f markdown -t html5 --citeproc -s -o docs/main.html --katex --default-image-extension ".svg"
-# mkdir docs/fig
-# cp fig/*.svg docs/fig
+# pandoc main.md -f markdown -t html5 --citeproc -s -o docs/main.html --katex --default-image-extension ".svg"
+Rscript -e 'stom::pandoc_html("main.md", "docs/main.html", c("--number-sections","--shift-heading-level-by=-1"))'
+[[ -d docs/fig ]] || mkdir docs/fig
+cp fig/*.svg docs/fig
+
+# , "-M", "link-citation=true"
